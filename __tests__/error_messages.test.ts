c2c4a2e3c30d836926e24e369359618f237a6abf
Johanna Elisabeth Sandbakken <johanna.sandbakken@gmail.com>
2020-09-21 17:39:09 +0200

    second testgreen third test red

--- __tests__/error_messages.test.ts
+++ __tests__/error_messages.test.ts
@@ -1,12 +1,22 @@
-const norwegian = {
+interface ApplicationTexts{
+    generalError: string
+}
+
+const norwegian:ApplicationTexts = {
     generalError: "Noe gikk galt"
+}
+const english:ApplicationTexts = {
+    generalError: "Something went wrong"
 }
 
 describe("error messages", () => {
-    it("showes error messages in norwegian", () => {
+    it("shows error messages in norwegian", () => {
         expect(norwegian.generalError).toEqual("Noe gikk galt")
     })
-    it("showes error messages in english", () => {
+    it("shows error messages in english", () => {
         expect(english.generalError).toEqual("Something went wrong")
+    })
+    it("shows error messages with code", () => {
+        expect(showError(norwegian,{errorCode:"NetworkError"})).toEqual("Problemer med å kontakte serveren")
     })
 })
