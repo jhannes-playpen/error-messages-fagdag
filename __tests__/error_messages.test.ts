1eb4ef55083cf9423e33c933825f091f1dc9b4d0
RAGNHILDSTVEIT Markus <m.ragnhildstveit@soprasteria.com>
2020-09-21 17:19:02 +0200

    First test green, second test red

--- __tests__/error_messages.test.ts
+++ __tests__/error_messages.test.ts
@@ -1,5 +1,12 @@
+const norwegian = {
+    generalError: "Noe gikk galt"
+}
+
 describe("error messages", () => {
     it("showes error messages in norwegian", () => {
         expect(norwegian.generalError).toEqual("Noe gikk galt")
+    })
+    it("showes error messages in english", () => {
+        expect(english.generalError).toEqual("Something went wrong")
     })
 })
